9551ac2611ab54769c091312bd7da475cda7e43d
hrt4423 <106893174+hrt4423@users.noreply.github.com>
2024-06-19 13:51:56 +0900

    wip

--- vanilla/index.js
+++ vanilla/index.js
@@ -5,7 +5,10 @@ document.addEventListener("DOMContentLoaded", function() {
   liff
     .init({ liffId: process.env.LIFF_ID })
     .then(() => {
-        console.log("Success! you can do something with LIFF API here.")
+        console.log("Success! you can do something with LIFF API here.");
+        console.log(liff.getLanguage());
+        console.log(liff.getOS());
+        console.log(liff.getVersion());
     })
     .catch((error) => {
         console.log(error)
