11f14b1cc1c28b4bd6295db0cb2ca23ecebcdf6f
hrt4423 <106893174+hrt4423@users.noreply.github.com>
2024-06-20 00:09:58 +0900

    wip

--- vanilla/index.js
+++ vanilla/index.js
@@ -1,7 +1,20 @@
-import './index.css';
+//import './index.css';
 import liff from '@line/liff'
 
+// element = document.getElementById("liff-message");
+// //liff-message の場所に "Hello, LIFF!" を表示
+// element.innerHTML = "Hello, LIFF!";
+
+
 document.addEventListener("DOMContentLoaded", function() {
+    // 特定の<p>タグを取得して内容を変更
+    const paragraph = document.getElementById('liff-message');
+    if (paragraph) {
+        paragraph.textContent = 'Hello, LIFF!';
+    } else {
+        console.error("Element with id 'liff-message' not found.");
+    }
+
   liff
     .init({ liffId: process.env.LIFF_ID })
     .then(() => {
@@ -14,3 +27,5 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log(error)
     })
 });
+
+
