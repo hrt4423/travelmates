c231f3c17d20e086c60f3dda66c1bb8841a9c4b2
2101032 <2101032@s.asojuku.ac.jp>
2024-07-08 10:15:11 +0900

    a

--- app/addplan.php
+++ app/addplan.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>予定追加</title>
-    <link rel="stylesheet" href="../app/addplan.css?<?php echo date('YmdHis'); ?>">
+    <link rel="stylesheet" href="../app/styles/addplan.css?<?php echo date('YmdHis'); ?>">
 </head>
 <body>
     <!-- ボタン -->
