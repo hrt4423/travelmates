31a8a712aa66875e2a89bc7108371d39b703a4f3
2101032 <2101032@s.asojuku.ac.jp>
2024-07-21 18:37:44 +0900

    a

--- app/addplan.php
+++ app/addplan.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>予定追加</title>
     <link rel="stylesheet" href="../app/styles/addplan.css?<?php echo date('YmdHis'); ?>">
 </head>
+
 <body>
     <!-- ボタン -->
     <button id="openModal">予定を追加する</button>
@@ -20,8 +22,8 @@
                 <div class="tab" data-target="modal-tab2">予定</div>
             </div>
             <div id="modal-tab1" class="tab-content active">
-                <h2>料金</h2>
-                
+                <h2>料金</h2><br>
+                <input type="text" placeholder="" style="width:35%;" name="budget">円
             </div>
             <div id="modal-tab2" class="tab-content">
                 <h2></h2>
@@ -32,4 +34,5 @@
 
     <script src="../app/scripts/addplan.js"></script>
 </body>
+
 </html>
