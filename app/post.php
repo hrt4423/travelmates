d448c2b3279cc6c02fff1ab58d0996ab4799fab0
2201111-kishikawa <114889553+2201111-kishikawa@users.noreply.github.com>
2024-09-17 10:35:36 +0900

    post_style.css作成

--- app/post.php
+++ app/post.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Travel Planner</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="post_styles.css">
 </head>
 <body>
     <header class="main-header">
@@ -80,6 +80,6 @@
 </div>
 
 
-    <script src="scripts.js"></script>
+    <script src="post_scripts.js"></script>
 </body>
 </html>
