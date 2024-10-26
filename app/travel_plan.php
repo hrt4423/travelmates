a7ce2297a00f28c096c9808483594d1383e7085b
hrt4423 <106893174+hrt4423@users.noreply.github.com>
2024-10-27 03:00:52 +0900

    wip

--- app/travel_plan.php
+++ app/travel_plan.php
@@ -10,8 +10,9 @@ session_start()
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>旅行計画</title>
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="../app/styles/addplan.css?<?php echo date('YmdHis'); ?>">
+  <link rel="stylesheet" href="../app/styles/travel_plan.css?<?php echo date('YmdHis'); ?>">
   <link rel="stylesheet" href="./styles/header.css" type="text/css">
+  <script src="./scripts/travel_pran.js" defer></script>
 </head>
 
 <body>
@@ -34,26 +35,74 @@ session_start()
     </nav>
   </header>
   <div class="container">
-
+    <div id="routes" class="row">
       <?php
-        //旅行データを取得して表示 作業者：平田
-        $travel_id = $_GET['travel_id'];
-        require_once('dao/travel.php');
-        $travel = new Travel();
-        $travel_data = $travel->findTravelByTravelId($travel_id);
+      $travel_id = $_SESSION['travel_id'];
+      $lastRouteNumber = 0;
 
-        echo("travel_id:".$travel_data['travel_id']."<br>");
-        echo("タイトル：".$travel_data['title']."<br>");
-        echo("作成者ID：".$travel_data['management_id']."<br>");
 
+      //旅行データを取得して表示 作業者：平田
+      $travel_id = $_GET['travel_id'];
+      require_once('dao/travel.php');
+      $travel = new Travel();
+      $travel_data = $travel->findTravelByTravelId($travel_id);
+
+      echo ("travel_id:" . $travel_data['travel_id'] . "<br>");
+      echo ("タイトル：" . $travel_data['title'] . "<br>");
+      echo ("作成者ID：" . $travel_data['management_id'] . "<br>");
+
+
+
+      try {
+        $pdo = new PDO('mysql:host=localhost;dbname=travelmates;charset=utf8', 'root', 'root');
+        $stmt = $pdo->prepare('SELECT * FROM event WHERE travel_id = ? ORDER BY route_id ASC');
+        $stmt->execute([$travel_id]);
+        $routes = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        if ($routes) {
+          $route_set = 999;
+          foreach ($routes as $route) {
+            $route_db = $route['route_id'];
+            $place = $route['place'];
+            $charge = $route['charge'];
+            $start = $route['start_datetime'];
+            $end = $route['end_datetime'];
+            $vehicle = $route['transport_id'];
+            if ($route_db == $route_set) {
+              echo "<div class='col-12 route' data-route-number='{$lastRouteNumber}'>
+                                <p>場所{$place}</p>                                
+                            ";
+            } else {
+              $lastRouteNumber += 1;
+              echo "<div class='col-12 route' data-route-number='{$lastRouteNumber}'>
+                                <p>ルート{$lastRouteNumber}</p>
+                                <p>場所{$place}</p>
+                            ";
+            }
+            $route_set = $route_db; //route_dbはデータベースから取得したルート番号 route_setは現在表示されている一番下のルート番号
+          }
+          if ($route_db == $route_set) {
+            echo "<button id='{$lastRouteNumber}' class='openModalButton'>予定を追加する</button></div>";
+          } else {
+            echo "<div>";
+          }
+        } else {
+          $lastRouteNumber = 0;
+          echo "<p>登録されているルートはありません。</p>";
+        }
+      } catch (PDOException $e) {
+        echo 'データベースエラー: ' . $e->getMessage();
+      }
       ?>
-
-
       <script>
         // PHPの変数をJavaScriptに渡す
         window.lastRouteNumber = <?php echo $lastRouteNumber; ?>;
       </script>
     </div>
+
+
+
+
 
     <button id="addRouteButton" class="btn btn-primary mt-3">新しいルートを追加する</button>
 
@@ -148,7 +197,6 @@ session_start()
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  <script src="../app/scripts/addplan.js"></script>
   <script src="./scripts/header.js"></script>
 </body>
 
