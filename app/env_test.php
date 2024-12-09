db5079f0399956b15f47d6ad3c3ac4b39412a015
hrt4423 <106893174+hrt4423@users.noreply.github.com>
2024-12-09 10:16:04 +0900

    wip

--- app/env_test.php
+++ app/env_test.php
@@ -2,5 +2,9 @@
   $username = "admin";
   
   print_r(getenv());
-  print_r(getenv('username'));
+  var_dump($_ENV);
+  print(getenv('USER_NAME'));
+  print(getenv('PASSWORD'));
+  print(getenv('DB_NAME'));
+  print(getenv('HOST_NAME'));
 ?>
